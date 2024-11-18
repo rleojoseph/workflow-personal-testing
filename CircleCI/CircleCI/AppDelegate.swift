5713abe920ecbc6cd9dcd3768770e00c2f13542e
Leo <leojoseph.r@rakuten.com>
2024-11-18 09:19:12 +0900

    Initial Commit

---
+++ CircleCI/CircleCI/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  CircleCI
+//
+//  Created by Leo, Joseph | Lj on 2024/11/18.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ CircleCI/CircleCI/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  CircleCI
+//
+//  Created by Leo, Joseph | Lj on 2024/11/18.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
